4eb420fc66f656b7614492e1f4fe639b41c8347f
skolar9 <56468180+skolar9@users.noreply.github.com>
2020-11-27 07:55:13 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -6,3 +6,15 @@ System. out. println("new branch commit changes");
 
 }
 }
+
+Commnads to Merge
+1.git clone https://url
+2.git checkout -b branch -> creates new branch from master
+3.git stash
+4.git checkout master
+5.git pull
+6.git checkout branch
+7.git stash pop
+8.git add .
+9.git commit -m ""
+10.git push
